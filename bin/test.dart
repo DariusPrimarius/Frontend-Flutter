8c07264b67b55e438547dbe6310c214cc828e450
DeCynamon <69729379+DeCynamon@users.noreply.github.com>
2022-11-17 06:39:03 +0100

    Communication with django

--- bin/test.dart
+++ bin/test.dart
@@ -1,1 +1,16 @@
+import 'dart:convert';
 
+import 'package:http/http.dart' as http;
+
+main() async {
+  //var response = await http.get(Uri.parse('http://127.0.0.1:8000/core/a/'));
+  var response = await http.post(Uri.parse('http://127.0.0.1:8000/core/a/'),
+      body: {
+        "username": "Dariusz",
+        "email": "dar@wp.pl",
+        "password": "qweqweqwe"
+      });
+
+  print(jsonDecode(response.body)["username"]);
+  print(jsonDecode(response.body)["email"]);
+}
